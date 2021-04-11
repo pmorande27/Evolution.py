7ad32f8f0165a0ea7967e907e15707e0cc57d78f
pmorande27 <pmorande27@gmail.com>
2021-04-12 01:40:36 +0200

    Move files to src

---
+++ src/population.py
@@ -0,0 +1,14 @@
+from individual import Individual
+class Population:
+    def __init__(self,number_of_individuals):
+        self.individuals = []
+        for i in range(number_of_individuals):
+            self.individuals.append(Individual())
+    def next_generation():
+        for individual in self.individuals:
+            if individual.death():
+                self.individuals.remove(individual)
+        new_generation = self.individuals[:]
+        for individual in self.individuals:
+            if individual.reproduction():
+                self.individuals.append(Individual())
