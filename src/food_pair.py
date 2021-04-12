7616f30959882fdcd7af7ea630ccb4db851486f7
pmorande27 <pmorande27@gmail.com>
2021-04-12 12:20:58 +0200

    Correct occupant condition

--- src/food_pair.py
+++ src/food_pair.py
@@ -2,6 +2,6 @@ class FoodPair(object):
     def __init__(self):
         self.occupants = []
     def is_occupied(self):
-        if len(self.occupants) < 2:
+        if len(self.occupants) == 2:
             return True
         return False
